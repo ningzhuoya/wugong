718248fdb713748b537d38db21b320f6abce73a5
machaochao <1053613529@qq.com>
2018-03-22 14:00:01 +0800

    增加一招

--- src/com/guigu/dugujiujian.java
+++ src/com/guigu/dugujiujian.java
@@ -5,5 +5,7 @@ public class dugujiujian {
 		System.out.println("111111111111");
 		System.out.println("222222222222");
 		System.out.println("333333333333");
+		System.out.println("444444444444");
+		System.out.println("555555555555");
 	}
 }
